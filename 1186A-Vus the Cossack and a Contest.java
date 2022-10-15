0efe8159972d56356c08d29de3a09e842ea19cd3
Nahida Sultana <102167959+nahida001@users.noreply.github.com>
2022-10-15 23:08:25 +0600

    Add files via upload

---
+++ 1186A-Vus the Cossack and a Contest.java
@@ -0,0 +1,16 @@
+package javaapplication84;
+import java.util.Scanner;
+public class JavaApplication84 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         int m=sc.nextInt();
+          int k=sc.nextInt();
+            if(n<=m&&n<=k){
+                System.out.println("YES");   
+          }else{
+                System.out.println("NO");
+            }
+    }
+    
+}
