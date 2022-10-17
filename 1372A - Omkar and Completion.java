1d0b1e3443bd9fa817b13af50e6f275dac0079c0
Nahida Sultana <102167959+nahida001@users.noreply.github.com>
2022-10-17 09:28:12 +0600

    Add files via upload

---
+++ 1372A - Omkar and Completion.java
@@ -0,0 +1,17 @@
+package javaapplication87;
+import java.util.Scanner;
+public class JavaApplication87 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t-->0){
+      int n=sc.nextInt();
+      for(int i=0;i<n;i++){
+        System.out.print(1+" ");
+                
+      }
+        System.out.println();
+    }
+    }
+    
+}
