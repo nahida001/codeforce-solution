907f2fd250f47ba5afa688c02cac51dc7ee8747e
Nahida Sultana <102167959+nahida001@users.noreply.github.com>
2022-11-05 09:08:34 +0600

    Add files via upload

---
+++ 1337A - Ichihime and Triangle.java
@@ -0,0 +1,17 @@
+package javaapplication89;
+import java.util.Scanner;
+public class JavaApplication89 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+          int a=sc.nextInt();
+          int b=sc.nextInt();
+          int c=sc.nextInt();
+          int d=sc.nextInt();
+            System.out.print(b+" "+c+" "+c);
+        System.out.println();
+        }
+    }
+    
+}
