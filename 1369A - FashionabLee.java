bc6debccd008fd5ca9b1997599192aaaba8f88c2
Nahida Sultana <102167959+nahida001@users.noreply.github.com>
2022-11-06 21:48:45 +0600

    Add files via upload

---
+++ 1369A - FashionabLee.java
@@ -0,0 +1,20 @@
+package javaapplication95;
+
+import java.util.Scanner;
+
+public class JavaApplication95 {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t-->0){
+        int temp=0;
+        int x=sc.nextInt();
+        if(x%4==0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+       }
+    }
+    
+}
