92de5c846ff840ec23642662de3b35febaddfc80
Nahida Sultana <102167959+nahida001@users.noreply.github.com>
2022-11-18 00:09:43 +0600

    Add files via upload

---
+++ 1553A - Digits Sum.java
@@ -0,0 +1,19 @@
+package javaapplication119;
+
+import java.util.Scanner;
+public class JavaApplication119 {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+              int t=sc.nextInt();
+              int r=0;
+             while(t-->0){
+                  int n=sc.nextInt();
+                n+=1;
+                r=n/10;
+                  System.out.println(r);
+                 
+                      
+          
+    }
+    } 
+}
