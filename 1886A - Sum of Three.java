1d62899d582132005a681d4ec12160ada33b5b24
Nahida Sultana <102167959+nahida001@users.noreply.github.com>
2023-11-08 10:59:08 +0600

    Add files via upload

---
+++ 1886A - Sum of Three.java
@@ -0,0 +1,25 @@
+
+package javaapplication37;
+
+import java.util.Scanner;
+
+public class JavaApplication37 {
+    public static void main(String[] args) {
+      Scanner sc= new Scanner(System.in) ;
+      int t=sc.nextInt();
+      while(t-->0){
+          int n=sc.nextInt();
+          if(n<7 || n==9){
+              System.out.println("NO");
+          }else{
+              System.out.println("YES");
+              if(n%3==0){
+                  System.out.println("1"+" "+"4"+" "+(n-5));
+              }else{
+                  System.out.println("1 "+"2 "+(n-3));
+              }
+          }
+      }
+    }
+    
+}
